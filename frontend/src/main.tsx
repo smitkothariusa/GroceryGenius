bafec4ccf7f02a7004465c4356f2fddfbf3d6df8
smitkothariusa <smitkothariusa@gmail.com>
2026-07-10 01:25:46 -0400

    feat: init global error handlers on app startup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import App from './App'
 import './mobile.css'
 import './mobile-responsive.css'
 import './i18n'
+import { initGlobalErrorHandlers } from './lib/errorService'
+
+initGlobalErrorHandlers()
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
